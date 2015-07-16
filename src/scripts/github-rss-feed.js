0018c522bd18eafaa7dd497a645c3814832e2055
goofiw <goofiw@gmail.com>
2015-07-16 09:44:35 -0700

    added in smaller github projects

--- src/scripts/github-rss-feed.js
+++ src/scripts/github-rss-feed.js
@@ -5,11 +5,13 @@ function initialize() {
   var feed = new google.feeds.Feed("https://github.com/goofiw.atom");
   feed.load(function(result) {
     if (!result.error) {
+      console.log(result);
       var container = document.getElementById('github-content');
       for (var i = 0; i < result.feed.entries.length; i++) {
         var entry = result.feed.entries[i];
         var div = document.createElement("div");
-        div.appendChild(document.createTextNode(entry.title));
+        div.innerHTML = '<a href=' + entry.link + '>' + entry.contentSnippet + '</a>';
+        //div.appendChild(document.createTextNode(entry.content));
         container.appendChild(div);
       }
     }
